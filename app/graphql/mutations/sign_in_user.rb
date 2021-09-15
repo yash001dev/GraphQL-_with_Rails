78f17d5b76e5f36dc5a397cb63d15ab02690606c
Yash Sanghavi <38398261+yash001dev@users.noreply.github.com>
2021-09-15 09:25:12 +0530

    Create Query Support for other Audiences Understanding.

--- app/graphql/mutations/sign_in_user.rb
+++ app/graphql/mutations/sign_in_user.rb
@@ -8,20 +8,29 @@ module Mutations
     field :user, Types::UserType, null: true
 
     def resolve(credentials: nil)
-      # basic validation
       return unless credentials
-
       user = User.find_by email: credentials[:email]
-
-      # ensures we have the correct user
       return unless user
       return unless user.authenticate(credentials[:password])
-
-      # use Ruby on Rails - ActiveSupport::MessageEncryptor, to build a token
       crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
-      token = crypt.encrypt_and_sign("user-id:#{user.id}")
-
+      token = crypt.encrypt_and_sign("user-id:#{user.id}"
       { user: user, token: token }
     end
   end
 end
+#We Want to query in graphiql then follow this
+#Addedd Query
+#   mutation{
+#   signinUser(input:{
+#     credentails:{
+#       email: "abc@gmail.com",
+#       password: "123456"
+#     }
+#   }){
+#     token
+#     user{
+#       id
+#     }
+#   }
+# }
+#End Of Query
